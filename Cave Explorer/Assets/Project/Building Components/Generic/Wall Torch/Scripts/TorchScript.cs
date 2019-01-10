44ea8cf4ac33455bb37061364340acf630d22534
Σωτήρης Τσεπελάκης <it134082@it.teithe.gr>
2019-01-10 23:31:03 +0200

    Add Torch Script

---
+++ Cave Explorer/Assets/Project/Building Components/Generic/Wall Torch/Scripts/TorchScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchScript : MonoBehaviour
+{
+    public ParticleSystem firePS;
+    public Light fireLight;
+
+    protected bool fireEnabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firePS.Stop();
+        fireLight.enabled = false;
+        fireEnabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!fireEnabled)
+        {
+            if (!firePS.isPlaying)
+            {
+                firePS.Play();
+                fireLight.enabled = true;
+                fireEnabled = true;
+            }
+        }
+        else
+        {
+            if (firePS.isPlaying)
+            {
+                firePS.Stop();
+                fireLight.enabled = false;
+                fireEnabled = false;
+            }
+        }
+    }
+}
